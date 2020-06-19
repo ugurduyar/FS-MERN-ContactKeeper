1c09da35c37fecededa2b6da715037b9b71bd979
ugurduyar <ismailugurduyar@gmail.com>
2020-06-19 22:13:05 +0300

    status bugfix

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,21 @@
-const jwt = require ('jsonwebtoken');
-const config = require('config');
-const { request } = require('express');
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { request } = require("express");
 
 module.exports = function (req, res, next) {
-    // Get token from header 
-    const token = req.header('x-auth-token');
+  // Get token from header
+  const token = req.header("x-auth-token");
 
-    // Check if not token 
-    if(!token) {
-        return res.status(401).json({msg:'No token, authorisation denied!'});
-    }
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorisation denied!" });
+  }
 
-    try {
-        const decoded = jsw.verify(token, config.get('jwtSecret'));
-        req.user = decoded.user;
-        next();
-    } catch (err) {
-        res.status(401).json({'Token is not valid!'});
-    }
-}
+  try {
+    const decoded = jsw.verify(token, config.get("jwtSecret"));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid!" });
+  }
+};
