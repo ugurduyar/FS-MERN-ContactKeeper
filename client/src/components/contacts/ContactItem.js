7891cf1954cbb9adb70b8fa837d6df96c609c837
ugurduyar <ismailugurduyar@gmail.com>
2020-06-22 14:34:37 +0300

    name div added to contactitem

---
+++ client/src/components/contacts/ContactItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ContactItem = ({ contact }) => {
+  const { id, name, email, phone, type } = contact;
+  return (
+    <div className="card bg-light">
+      <h3 className="text-primary text-left">{name}</h3>
+    </div>
+  );
+};
+
+export default ContactItem;
