9b24f84172dff2516c249bfccf6bad85fdca54f2
ugurduyar <ismailugurduyar@gmail.com>
2020-06-30 18:58:21 +0300

    filter contact added to home screen

--- client/src/components/contacts/ContactFilter.js
+++ client/src/components/contacts/ContactFilter.js
@@ -1,8 +1,34 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef, useEffect } from "react";
 import ContactContext from "../../context/contact/contactContext";
 const ContactFilter = () => {
   const contactContext = useContext(ContactContext);
-  return <div></div>;
+  const text = useRef("");
+
+  const { filterContacts, clearFilter, filtered } = contactContext;
+  useEffect(() => {
+    if (filtered === null) {
+      text.current.value = "";
+    }
+  });
+
+  const onChange = (e) => {
+    if (text.current.value !== "") {
+      filterContacts(e.target.value);
+    } else {
+      clearFilter();
+    }
+  };
+
+  return (
+    <form>
+      <input
+        ref={text}
+        type="text"
+        placeholder="Filter Contacts..."
+        onChange={onChange}
+      />
+    </form>
+  );
 };
 
 export default ContactFilter;
