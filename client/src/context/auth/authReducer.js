d93b80309cf8af636eacf8ede1c3fb36b08414ab
ugurduyar <ismailugurduyar@gmail.com>
2020-07-01 23:48:27 +0300

    register fail state added

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -19,6 +19,16 @@ export default (state, action) => {
         isAuthenticated: true,
         loading: false,
       };
+    case REGISTER_FAIL:
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null,
+        error: action.payload,
+      };
     default:
       return state;
   }
