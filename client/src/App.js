66eff0eec71e2b7dd1743b8f003d5030df42bd3b
ugurduyar <ismailugurduyar@gmail.com>
2020-07-01 19:29:25 +0300

    alert state imported and wrapped around app

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Login from "./components/auth/Login";
 
 import ContactState from "./context/contact/ContactState";
 import AuthState from "./context/auth/AuthState";
+import AlertState from "./context/alert/AlertState";
 
 import "./App.css";
 
@@ -15,19 +16,21 @@ const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route exact path="/About" component={About} />
-                <Route exact path="/Register" component={Register} />
-                <Route exact path="/Login" component={Login} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className="container">
+                <Switch>
+                  <Route exact path="/" component={Home} />
+                  <Route exact path="/About" component={About} />
+                  <Route exact path="/Register" component={Register} />
+                  <Route exact path="/Login" component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
       </ContactState>
     </AuthState>
   );
