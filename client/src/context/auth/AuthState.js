cbe9ed8501c1f48a398ccb4306606a5e9101f4a9
ugurduyar <ismailugurduyar@gmail.com>
2020-07-02 21:43:23 +0300

    local storage token added

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -2,6 +2,7 @@ import React, { useReducer } from "react";
 import axios from "axios";
 import AuthContext from "./authContext";
 import authReducer from "./authReducer";
+import setAuthToken from "../../utils/setAuthToken";
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
@@ -27,7 +28,9 @@ const AuthState = (props) => {
   // Load user
 
   const loadUser = async () => {
-    // @todo - load token into global headers
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
     try {
       const res = await axios.get("/api/auth");
       dispatch({ type: USER_LOADED, payload: res.data });
