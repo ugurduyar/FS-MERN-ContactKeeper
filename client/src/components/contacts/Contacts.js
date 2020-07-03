fd91a9e71ee722ccccc1e0ec19cddbedf05dfff3
İsmail Uğur Duyar <ismailugurduyar@gmail.com>
2020-07-03 20:22:13 +0300

    useEffect added to contact js

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,11 +1,17 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, useEffect } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import ContactItem from "./ContactItem";
 import ContactContext from "../../context/contact/contactContext";
 
 function Contacts() {
   const contactContext = useContext(ContactContext);
-  const { contacts, filtered } = contactContext;
+  
+  const { contacts, filtered, getContacts, loading } = contactContext;
+
+  useEffect (() => {
+    getContacts();
+    // eslint-disable-next-line
+  }, [])
 
   if (contacts.length === 0) {
     return <h4>Please add a contact</h4>;
